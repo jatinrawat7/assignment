dd85309ebc908c3da77767262d090caedb3fa782
jatinrawat7 <84801953+jatinrawat7@users.noreply.github.com>
2021-05-28 16:54:02 +0530

    new commit

---
+++ Challenge1/Studentmanager/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Studentmanager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of students");
+            var studentcount = int.Parse(Console.ReadLine());
+
+            var studentgrades = new int[studentcount];
+            var studentnames = new String[studentcount];
+
+            for (int i = 0; i < studentcount; i++)
+            {
+                Console.Write("Enter Name of student: ");
+                studentnames[i] = Console.ReadLine();
+
+                Console.Write("Enter Grades of student: ");
+                studentgrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentcount; i++)
+            {
+                Console.WriteLine("Name of student: {0}, Grade: {1}", studentnames[i], studentgrades[i]);
+            }
+        }
+    }
+}
